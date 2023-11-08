fed96df3e5a86ddd6aa70f39b571c669de3804be
Jason Bozoki <543468@edu.rocmn.nl>
2023-11-08 14:14:01 +0100

    file upload edit
    file upload may not be entirely functioning, honestly got no clue as to what happened between database switches but it just broke :(
    
    Logics behind everything should be working just fine tho

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# mvc-example
-Small exanoke if a mvc system built from the ground up
+file upload may not be entirely functioning, honestly got no clue as to what happened between database switches but it just broke :( 
+
+Logics behind everything should be working just fine tho
--- model/ProductsLogic.php
+++ model/ProductsLogic.php
@@ -16,62 +16,62 @@ class ProductsLogic
   public function createProduct($product_type_code, $supplier_id, $product_name, $product_price, $product_thumbnail, $other_product_details)
   {
 
-    // try {
-      // $target_dir = "view/assets/image/";
-      // $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-      // $uploadOk = 1;
-      // $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-      // // Check if image file is a actual image or fake image
-      // if (isset($_POST["submit"])) {
-      //   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-      //   if ($check !== false) {
-      //     echo "File is an image - " . $check["mime"] . ".";
-      //     $uploadOk = 1;
-      //   } else {
-      //     echo "File is not an image.";
-      //     $uploadOk = 0;
-      //   }
-      // }
-      // // Check if file already exists
-      // if (file_exists($target_file)) {
-      //   echo "Sorry, file already exists.";
-      //   $uploadOk = 0;
-      // }
+    try {
+      $target_dir = "view/assets/image/";
+      $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+      $uploadOk = 1;
+      $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+      // Check if image file is a actual image or fake image
+      if (isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if ($check !== false) {
+          echo "File is an image - " . $check["mime"] . ".";
+          $uploadOk = 1;
+        } else {
+          echo "File is not an image.";
+          $uploadOk = 0;
+        }
+      }
+      // Check if file already exists
+      if (file_exists($target_file)) {
+        echo "Sorry, file already exists.";
+        $uploadOk = 0;
+      }
 
-      // // Check file size
-      // if ($_FILES["fileToUpload"]["size"] > 500000) {
-      //   echo "Sorry, your file is too large.";
-      //   $uploadOk = 0;
-      // }
+      // Check file size
+      if ($_FILES["fileToUpload"]["size"] > 500000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+      }
 
-      // // Allow certain file formats
-      // if (
-      //   $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-      //   && $imageFileType != "gif"
-      // ) {
-      //   echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-      //   $uploadOk = 0;
-      // }
+      // Allow certain file formats
+      if (
+        $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+        && $imageFileType != "gif"
+      ) {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+      }
 
-      // // Check if $uploadOk is set to 0 by an error
-      // if ($uploadOk == 0) {
-      //   echo "Sorry, your file was not uploaded.";
-      //   // if everything is ok, try to upload file
-      // } else {
-      //   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-      //     $sql = "INSERT INTO `products`(`product_id`, `product_type_code`, `supplier_id`, `product_name`, `product_price`, `product_thumbnail`, `other_product_details`) VALUES (NULL, '{$product_type_code}', '{$supplier_id}', '{$product_name}', '{$product_price}', '{$_FILES["fileToUpload"]["name"]}', '{$other_product_details}')";
-      //     echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.";
-      //   } else {
-      //     echo "Sorry, there was an error uploading your file.";
-      //   }
-      // }
-      // $sql = "INSERT INTO `products`(`product_id`, `product_type_code`, `supplier_id`, `product_name`, `product_price`, `product_thumbnail`, `other_product_details`) VALUES (NULL, '{$product_type_code}', '{$supplier_id}', '{$product_name}', '{$product_price}', '{$_FILES["fileToUpload"]["name"]}', '{$other_product_details}')";
-      $sql = "INSERT INTO `products`(`product_id`, `product_type_code`, `supplier_id`, `product_name`, `product_price`, `other_product_details`) VALUES (NULL, '{$product_type_code}', '{$supplier_id}', '{$product_name}', '{$product_price}', '{$other_product_details}')";
+      // Check if $uploadOk is set to 0 by an error
+      if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+        // if everything is ok, try to upload file
+      } else {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+          $sql = "INSERT INTO `products`(`product_id`, `product_type_code`, `supplier_id`, `product_name`, `product_price`, `product_thumbnail`, `other_product_details`) VALUES (NULL, '{$product_type_code}', '{$supplier_id}', '{$product_name}', '{$product_price}', '{$_FILES["fileToUpload"]["name"]}', '{$other_product_details}')";
+          echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.";
+        } else {
+          echo "Sorry, there was an error uploading your file.";
+        }
+      }
+      $sql = "INSERT INTO `products`(`product_id`, `product_type_code`, `supplier_id`, `product_name`, `product_price`, `product_thumbnail`, `other_product_details`) VALUES (NULL, '{$product_type_code}', '{$supplier_id}', '{$product_name}', '{$product_price}', '{$_FILES["fileToUpload"]["name"]}', '{$other_product_details}')";
+      // $sql = "INSERT INTO `products`(`product_id`, `product_type_code`, `supplier_id`, `product_name`, `product_price`, `other_product_details`) VALUES (NULL, '{$product_type_code}', '{$supplier_id}', '{$product_name}', '{$product_price}', '{$other_product_details}')";
       $results = $this->DataHandler->createData($sql);
       return $results;
-    // } catch (Exception $e) {
-    //   throw $e;
-    // }
+    } catch (Exception $e) {
+      throw $e;
+    }
   }
 
   public function readProducts($product_id)
